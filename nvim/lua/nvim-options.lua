0ccb1645295fa0fb91fb76eb4fb11d9a7b5ff81c
Infinity <infinity9151@gmail.com>
2025-02-10 01:36:14 +0530

    Nvim uses system clipboard

--- nvim/lua/nvim-options.lua
+++ nvim/lua/nvim-options.lua
@@ -14,7 +14,8 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = "a"
 vim.o.showmode = false
---vim.o.clipboard = "unnamed"
+-- Use System Clipboard
+vim.o.clipboard = "unnamedplus"
 
 vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
