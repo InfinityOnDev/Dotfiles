a787a14fdae235bde8b6a498fd8f0ae2a6a09f85
Infinity <infinity9151@gmail.com>
2025-05-04 11:15:31 +0530

    Add rust to treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-			ensure_installed = { "c", "cpp", "vim", "vimdoc", "lua", "python", "latex" },
+			ensure_installed = { "c", "cpp", "vim", "vimdoc", "lua", "python", "rust" },
 			sync_install = false,
 			auto_install = false,
 			highlight = { enable = true },
