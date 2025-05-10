a740e4b78cf361b2cf89a513c20fed1326816564
Infinity <infinity9151@gmail.com>
2025-05-10 20:13:49 +0530

    changed alpha.nvim to comply with new nvim standards

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,32 +1,16 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local alpha = require("alpha")
-		local startify = require("alpha.themes.startify")
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local alpha = require("alpha")
+    local startify = require("alpha.themes.startify")
 
-		-- Replace the default header with your custom ASCII art
-		startify.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
-
-		alpha.setup(startify.config)
-	end,
+    -- No need to set a custom header; the default one will be used
+    -- Just set up the theme as-is
+    alpha.setup(startify.config)
+  end,
+  event = "VimEnter", -- optional, lazy-load on startup
 }
 
