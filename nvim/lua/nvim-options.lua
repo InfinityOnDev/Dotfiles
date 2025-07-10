355c0b0171984c0e0402f7acb6cf3e81ddd4b13a
Infinity <infinity9151@gmail.com>
2025-07-10 13:10:16 +0530

    Add encoding option to 'nvim-options.lua'

--- nvim/lua/nvim-options.lua
+++ nvim/lua/nvim-options.lua
@@ -33,3 +33,6 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- set encoding 
+vim.opt.encoding = "UTF-8"
