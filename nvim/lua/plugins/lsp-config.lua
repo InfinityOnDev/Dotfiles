917ce6229a113a3a3846068492a7ea6c4df088cc
Infinity <infinity9151@gmail.com>
2025-07-23 16:38:36 +0530

    Remove bashls from LSP list

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,7 +22,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd", "ruff", "bashls", },
+        ensure_installed = { "lua_ls", "pyright", "clangd", "ruff", },
       })
     end,
   },
