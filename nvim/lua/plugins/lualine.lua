b3313f65e2d51b469f5722d98de963315b4f4c03
Infinity <infinity9151@gmail.com>
2025-10-30 18:11:25 +0530

    add diagnostics and diffs to lualine.lua (nvim)

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,23 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  priority = 100,
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "auto",
-        section_separators = "",
-        component_separators = "",    -- removes vertical bars
-        globalstatus = true,          -- optional: cleaner look in newer Neovim
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	priority = 100,
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+				section_separators = "",
+				component_separators = "", -- removes vertical bars
+				globalstatus = true, -- optional: cleaner look in newer Neovim
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
-
