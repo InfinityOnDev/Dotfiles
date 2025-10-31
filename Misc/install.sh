19ba7f75f75a2eafb5e7c1cc3c991567ea85cd48
Infinity <infinity9151@gmail.com>
2025-10-31 19:40:25 +0530

    Add mpv back to tools.md and install.sh

--- Misc/install.sh
+++ Misc/install.sh
@@ -1,2 +1,2 @@
-sudo dnf install neovim alacritty btop git clang cargo npm pipx wget ripgrep fd-find zoxide wl-clipboard gnome-tweaks cronie zathura zathura-pdf-mupdf zathura-djvu
+sudo dnf install neovim alacritty btop git clang cargo npm pipx wget ripgrep fd-find zoxide wl-clipboard gnome-tweaks mpv cronie zathura zathura-pdf-mupdf zathura-djvu
 
