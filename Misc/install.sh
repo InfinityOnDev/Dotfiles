9b17bf3e8ace3755cfc5030307fd01ae967ec832
Infinity <infinity9151@gmail.com>
2026-01-18 07:42:05 +0530

    remove whitespace

--- Misc/install.sh
+++ Misc/install.sh
@@ -2,7 +2,6 @@
 sudo dnf install neovim alacritty btop git clang cargo npm pipx wget ripgrep fd-find zoxide wl-clipboard gnome-tweaks mpv cronie zathura zathura-pdf-mupdf zathura-djvu
 
 # getting cool wallpapers
-#
 mkdir -p ~/Pictures/Wallpapers/
 cd ~/Pictures/Wallpapers/
 git clone https://github.com/mylinuxforwork/wallpaper
