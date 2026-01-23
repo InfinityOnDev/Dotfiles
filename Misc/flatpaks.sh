aa3ef0e45074124f6939875849c56c3ac0daf346
Infinity <infinity9151@gmail.com>
2026-01-23 16:15:21 +0530

    add installation script for flatpaks

---
+++ Misc/flatpaks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+response='N'
+echo 'Begin flatpak tools installation? (y/N):'
+read response
+
+if [[ $response == 'y' ]] ; then
+  flatpak install -y \
+    io.github.flattool.Warehouse/x86_64/stable \
+    app/md.obsidian.Obsidian/x86_64/stable \
+    one.ablaze.floorp/x86_64/stable \
+    app/com.google.Chrome/x86_64/stable \
+    app/com.dec05eba.gpu_screen_recorder/x86_64/stable
+  echo 'Installation successful!'
+  exit 0
+else 
+  echo 'Installation aborted!'
+  exit 1
+fi 
+
